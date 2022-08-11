e9667101653a890c5d8d67147f6286ccd0de834e
DarthThrasher <jozeamg@gmail.com>
2022-08-11 15:15:38 -0500

    done with excer 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,28 @@
-const reverseString = function() {
+const reverseString = function(str) {
+        // Check input
+        /*if(!str || str.length < 0 ||
+                typeof str!== 'string') {
+            return 'Not valid';
+        }*/
+         
+        // Take empty array revArray
+        const revArray = [];
+        const length = str.length - 1;
+         
+        // Looping from the end
+        for(let i = length; i >= 0; i--) {
+            revArray.push(str[i]);
+        }
+         
+        // Joining the array elements
+        return revArray.join('');
 
 };
+reverseString('hello');
+reverseString('hello there');
+reverseString('123! abc!');
+reverseString('');
+
 
 // Do not edit below this line
 module.exports = reverseString;
